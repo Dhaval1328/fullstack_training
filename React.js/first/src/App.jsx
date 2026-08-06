9df783c7f50fdeeffca44165918c761a283ceccc
Dhaval1328 <ddhaval2655@gmail.com>
2026-08-06 08:17:56 +0530

    React.js

--- React.js/first/src/App.jsx
+++ React.js/first/src/App.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
 function App() {
+  const [count, setcount] = useState(0);
+
+  function decrement() {
+    setcount(count - 1);
+  }
+  function increment() {
+    setcount(count + 1);
+  }
+  function reset() {
+    setcount(0);
+  }
   return (
     <div>
-      <h1>This is Heading</h1>
-      <h1>This is Heading</h1>
-      <h1>This is Heading</h1>
+      <h1>{count}</h1>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={increment}>Increment</button>
     </div>
   );
 }
