6aff988be3f2d6ee3d21f34973c74bcb5941ac95
Dhaval1328 <ddhaval2655@gmail.com>
2026-08-08 15:22:44 +0530

    Day-9

--- Project1/src/App.jsx
+++ Project1/src/App.jsx
@@ -2,9 +2,11 @@ import Example_1 from "./React_Props/Example_1";
 
 function App() {
   let name = "Dhaval";
+  let age = 20;
+
   return (
     <>
-      <Example_1 name={name} />
+      <Example_1 name={name} age={age} />
     </>
   );
 }
--- Project1/src/React_Props/Example_1.jsx
+++ Project1/src/React_Props/Example_1.jsx
@@ -1,7 +1,18 @@
+// function Example_1(props) {
+//   return (
+//     <>
+//       <h1>Name is : {props.name}</h1>
+//     </>
+//   );
+// }
+
+// export default Example_1;
+
 function Example_1(props) {
   return (
     <>
-      <h1>Name is : {props.name}</h1>
+      <h1>Name is: {props.name}</h1>
+      <h1>Age is: {props.age}</h1>
     </>
   );
 }
