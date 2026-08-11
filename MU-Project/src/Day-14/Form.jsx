a7cc356fade5ee9e7a1820e591a119ebedd7f758
Dhaval1328 <ddhaval2655@gmail.com>
2026-08-11 08:20:41 +0530

    Day-11

--- MU-Project/src/Day-14/Form.jsx
+++ MU-Project/src/Day-14/Form.jsx
@@ -1,10 +1,27 @@
 import { useState } from "react";
 function Form() {
   const [name, setName] = useState("");
+
+  const hendalName = (e) => {
+    //console.log(e);
+    //console.log(e.target);
+    //console.log(e.target.value);
+    setName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name);
+  };
+
   return (
     <>
-      <label htmlFor="">Enter Name :</label>
-      <input type="text" value={name} onChange={} />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="">Enter Name : </label>
+        <input type="text" value={name} onChange={hendalName} />
+        <h1>{name}</h1>
+        <button type="Submit">Submit</button>
+      </form>
     </>
   );
 }
