3398eed48369ac80d4af531fb2f85d79d25f4f32
Dhaval1328 <ddhaval2655@gmail.com>
2026-08-13 08:44:41 +0530

    Day-13

--- MU-Project/src/App.jsx
+++ MU-Project/src/App.jsx
@@ -1,10 +1,11 @@
-// import Flipper from "./Color_Fliper/Flipper";
+import Flipper from "./Color_Fliper/Flipper";
 import Form from "./Day-14/Form";
 import Form2 from "./Day-14/Form2";
 import Loginform from "./Day-14/Loginform";
 import Review from "./Review/Review";
 import Navbar from "./Navbar/Navbar";
 import ArrayUpdate from "./Array/ArrayUpdate";
+import Cart from "./Shopping/Cart";
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
       {/* <Loginform /> */}
       {/* <Review /> */}
       {/* <Navbar /> */}
-      <ArrayUpdate />
+      {/* <ArrayUpdate /> */}
+      <Cart />
     </>
   );
 }
---
+++ MU-Project/src/Shopping/Cart.jsx
@@ -0,0 +1,30 @@
+let cart = [
+  {
+    id: 1,
+    name: "Laptop",
+    price: 50000,
+    quantity: 1,
+  },
+  {
+    id: 2,
+    name: "Mobile",
+    price: 50000,
+    quantity: 1,
+  },
+  {
+    id: 3,
+    name: "TV",
+    price: 50000,
+    quantity: 1,
+  },
+];
+
+function Cart() {
+  return (
+    <>
+      <h1>Cart Page</h1>
+    </>
+  );
+}
+
+export default Cart;
