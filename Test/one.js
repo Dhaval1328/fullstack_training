31626fb8c26c4900471f655a0356bf5dfbf2c4f6
Dhaval1328 <ddhaval2655@gmail.com>
2026-08-22 15:02:58 +0530

    Backend

---
+++ Test/one.js
@@ -0,0 +1,3 @@
+let name = "Dhaval";
+
+export default name;
---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import http from 'http';
+
+const PORT = 5000;
+
+const app = http.createServer((req, res) => {
+    res.end("This is my First server");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is Running At http://localhost:${PORT}`);
+});
