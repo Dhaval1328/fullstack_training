1c74fa79866b0bdf3459fde652acecb475d55a62
Dhaval1328 <ddhaval2655@gmail.com>
2026-08-25 07:54:59 +0530

    file Sysytem

--- backend/File System/crud.js
+++ backend/File System/crud.js
@@ -2,3 +2,9 @@
 
 
 import fs from 'fs';
+
+// reading a file:
+const data = fs.readFileSync('./read.txt');  //read file and generates the buffer data
+const data1 = fs.readFileSync('./read.txt','utf-8'); // read file and generates the actual data 
+console.log(data)
+console.log(data1)
