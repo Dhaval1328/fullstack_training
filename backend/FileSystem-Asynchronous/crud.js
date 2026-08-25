a9ce0bd78c59372258ba60fe924902446b75530a
Dhaval1328 <ddhaval2655@gmail.com>
2026-08-25 08:56:56 +0530

    file Sysytem

---
+++ backend/FileSystem-Asynchronous/crud.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+
+// // Create a directory
+// fs.mkdir("test", (err) => {
+
+//     if (err) {
+//         throw err;
+//     }
+
+//     console.log("Directory Created...");
+// });
+
+fs.readdir("F:\\MERN_Training\\backend\\FileSystem-Asynchronous", (err, data) => {
+
+    if (err) {
+        throw err;
+    }
+
+    console.log(data);
+
+    for (let x of data) {
+        console.log(x);
+    }
+
+});
