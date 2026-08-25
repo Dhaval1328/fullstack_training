b21660da74a54bdec3c1da51185e00312a7b4624
Dhaval1328 <ddhaval2655@gmail.com>
2026-08-25 08:13:52 +0530

    file Sysytem

--- backend/File System/crud.js
+++ backend/File System/crud.js
@@ -3,22 +3,25 @@
 
 import fs from 'fs';
 
-// // reading a file:
-// const data = fs.readFileSync('./read.txt');  //read file and generates the buffer data
-// const data1 = fs.readFileSync('./read.txt','utf-8'); // read file and generates the actual data 
-// console.log(data)
-// console.log(data1)
+//how to create a file:
+fs.writeFileSync('./read.txt',"Hell This Is My read.txt file")
+
+// reading a file:
+const data = fs.readFileSync('./read.txt');  //read file and generates the buffer data
+const data1 = fs.readFileSync('./read.txt','utf-8'); // read file and generates the actual data 
+console.log(data)
+console.log(data1)
 
 
-// //appending a file :
-// fs.appendFileSync('./read.txt', "I am learning How TO Uppend Text In fs")
+//appending a file :
+fs.appendFileSync('./read.txt', "I am learning How TO Uppend Text In fs")
 
-// const data2 = fs.readFileSync('./read.txt','utf-8');
-// console.log(data2)
+const data2 = fs.readFileSync('./read.txt','utf-8');
+console.log(data2)
 
 
-// //rename a file:
-// fs.renameSync("./read.txt","ABC.txt")
+//rename a file:
+fs.renameSync("./read.txt","ABC.txt")
 
 
 fs.unlinkSync("./ABC.txt");
