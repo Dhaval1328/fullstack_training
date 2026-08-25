abcd6da82866849082e81a178ad21b0fe8a4f0bc
Dhaval1328 <ddhaval2655@gmail.com>
2026-08-25 09:16:20 +0530

    file Sysytem

--- backend/FileSystem-Asynchronous/crud.js
+++ backend/FileSystem-Asynchronous/crud.js
@@ -10,26 +10,56 @@ import fs from 'fs';
 //     console.log("Directory Created...");
 // });
 
-fs.readdir("F:\\MERN_Training\\backend\\FileSystem-Asynchronous", (err, data) => {
+// fs.readdir("F:\\MERN_Training\\backend\\FileSystem-Asynchronous", (err, data) => {
 
+//     if (err) {
+//         throw err;
+//     }
+
+//     console.log(data);
+
+//     for (let x of data) {
+//         console.log(x);
+//     }
+
+// });
+
+// fs.rmdir("test", (err) => {
+
+//     if (err) {
+//         throw err;
+//     }
+
+//     console.log("Directory Removed...");
+
+// });
+
+
+import fs from "fs";
+
+// Write file
+fs.writeFile("./abc.txt", "Hello Good Morning", (err) => {
+    if (err) {
+        throw err;
+    }
+
+    console.log("File Created Successfully");
+
+    // Read file
+fs.readFile("./abc.txt", "utf-8", (err, data) => {
     if (err) {
         throw err;
     }
 
     console.log(data);
 
-    for (let x of data) {
-        console.log(x);
-    }
-
-});
-
-fs.rmdir("test", (err) => {
-
+        // Append file
+fs.appendFile("./abc.txt", "\nTea Is Good", (err) => {
     if (err) {
         throw err;
     }
 
-    console.log("Directory Removed...");
-
+    console.log("Data Appended Successfully");
+});
+});
 });
