ea1e4be36b1eb680f7fe4467387e2cd43c0dcb6b
Dhaval1328 <ddhaval2655@gmail.com>
2026-08-25 08:04:45 +0530

    file Sysytem

--- backend/File System/crud.js
+++ backend/File System/crud.js
@@ -3,15 +3,19 @@
 
 import fs from 'fs';
 
-// reading a file:
-const data = fs.readFileSync('./read.txt');  //read file and generates the buffer data
-const data1 = fs.readFileSync('./read.txt','utf-8'); // read file and generates the actual data 
-console.log(data)
-console.log(data1)
+// // reading a file:
+// const data = fs.readFileSync('./read.txt');  //read file and generates the buffer data
+// const data1 = fs.readFileSync('./read.txt','utf-8'); // read file and generates the actual data 
+// console.log(data)
+// console.log(data1)
 
 
-//appending a file :
-fs.appendFileSync('./read.txt', "I am learning How TO Uppend Text In fs")
+// //appending a file :
+// fs.appendFileSync('./read.txt', "I am learning How TO Uppend Text In fs")
 
-const data2 = fs.readFileSync('./read.txt','utf-8');
-console.log(data2)
+// const data2 = fs.readFileSync('./read.txt','utf-8');
+// console.log(data2)
+
+
+//rename a file:
+fs.renameSync("./read.txt","ABC.txt")
