aa40752ab42222c31d15e14c2a1dc7fed6bd4b27
Dhaval1328 <ddhaval2655@gmail.com>
2026-08-27 08:28:52 +0530

    last

--- backend/lastnode/index.js
+++ backend/lastnode/index.js
@@ -1,6 +1,19 @@
-import os from 'os'
+// import os from 'os'
 
 
-console.log(os)
-console.log("Architecture is",os.arch());
+// console.log(os)
+// console.log("Architecture is",os.arch());
+// console.log("How Many Core In My Computer",os.cpus().length);
 
+import express from 'express'
+
+const app = express();
+const PORT = 6060;
+
+app.get('/', (req, res) => {
+    res.json("This Is My Express Server");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server Is Running At http://localhost:${PORT}`);
+});
